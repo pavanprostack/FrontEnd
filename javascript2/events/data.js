3469e45ee6eed79408445bfcb7bf7e495e4a6500
pavanprostack <pavanprostack@gmail.com>
2022-08-22 23:34:36 +0530

    okkkk

--- javascript2/events/data.js
+++ javascript2/events/data.js
@@ -1,27 +1,27 @@
 
 
 function on_mouseOver(){
-    document.getElementById('mouseOver').style.backgroundColor="blue"
+    document.getElementById('mouseOver').style.backgroundColor="blue";
 }
 
 function on_mouseOut(){
-    document.getElementById('mouseOut').style.backgroundColor="green"
+    document.getElementById('mouseOut').style.backgroundColor="green";
 }
 
-function on_Click(){
-    document.getElementById("click_Handler").style.backgroundColor="red"
+function changeColor(){
+    document.getElementById('king').style.backgroundColor="red"
 }
 
 function onDoubleClick(){
-    document.getElementById("doubleClick_Handler").style.backgroundColor="pink"
+    document.getElementById("doubleClick").style.backgroundColor="pink";
 }
 
 function onFocus(){
-    document.getElementById("focus").style.backgroundColor="aqua"
+    document.getElementById("focus").style.backgroundColor="aqua";
 }
 
 function onBlur_Handler(){
-    let name= document.getElementById("blur").value 
+    let name = document.getElementById("blur").value ;
     console.log(name);
-    name = name.touppercase();
+    document.getElementById("blur").value = name.toUpperCase();
 }
