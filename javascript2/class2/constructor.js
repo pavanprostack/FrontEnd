c4b6173409d518da650703255d7865eedbbf21ba
pavanprostack <pavanprostack@gmail.com>
2022-08-27 00:43:34 +0530

    constructor

---
+++ javascript2/class2/constructor.js
@@ -0,0 +1,37 @@
+
+
+class Employee{
+    constructor(){
+        console.log("Subbu");
+
+    }
+}
+let e=new Employee();
+console.log(e);
+
+
+
+
+
+
+class Student{
+    sId;
+    sName;
+    sFee;
+    constructor(id,name,fee){
+        this.sId=id;
+        this.sName=name;
+        this.sFee=fee;
+    }
+    getDetails(){
+        return "pavankalyan"
+    }
+}
+
+let s=new Student(101,"pavan",30000);
+console.log(s);
+let s1=new Student(102,"sai",40000);
+console.log(s1);
+console.log( s.getDetails());
+
+
