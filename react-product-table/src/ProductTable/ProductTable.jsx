eccf3288c1285b726fbb2ca23aae1c934cae5336
pavanprostack <pavanprostack@gmail.com>
2022-09-19 22:55:50 +0530

    produtable

--- react-product-table/src/ProductTable/ProductTable.jsx
+++ react-product-table/src/ProductTable/ProductTable.jsx
@@ -27,7 +27,7 @@ class ProductTable extends React.Component{
     render(){
         return <div className="container mt-5">
             <div className="row">
-                <div className="col">
+                <div className="col-md-6">
                     <table className='table table-hover'>
                         <thead className='bg-primary'>
                             <th>Name</th>
@@ -35,6 +35,7 @@ class ProductTable extends React.Component{
                             <th>Price</th>
                             <th>Model</th>
                             <th>qty</th>
+                            <th>Total</th>
                         </thead>
                         <tbody>
                             <tr>
@@ -43,6 +44,7 @@ class ProductTable extends React.Component{
                                 <td>{this.state.product.price}</td>
                                 <td>{this.state.product.model}</td>
                                 <td><i className='fa fa-minus-circle' onClick={this.decreHandler}></i> {this.state.product.qty} <i className='fa fa-plus-circle' onClick={this.increHandler}></i></td>
+                                <td>{this.state.product.qty * this.state.product.price}</td>
                             </tr>
                         </tbody>
                     </table>
