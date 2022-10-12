66645962d8eb91791297323d31567a6b880a2d02
pavanprostack <pavanprostack@gmail.com>
2022-10-13 00:34:56 +0530

    redux combineReducers

---
+++ redux-3/src/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import {Link} from 'react-router-dom'
+
+// const Navbar = () => {
+//   return <>
+//   <nav className='navbar navbar-dark bg-success'>
+//     <Link to="#" className='navbar-brand'>Redux Full Concept</Link>
+//     <div className='ml-auto'>
+//       <ul className='navbar-nav'>
+//         <li className='nav-list'><Link to="message" className='nav-link'>Message</Link></li>
+//       </ul>
+//     </div>
+//   </nav>
+//   </>
+// }
+
+// export default Navbar
