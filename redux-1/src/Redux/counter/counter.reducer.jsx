7bcd4609aa9622afcdb1eeb0a234d1f018f5d122
pavanprostack <pavanprostack@gmail.com>
2022-10-12 12:54:27 +0530

    bootstrap

--- redux-1/src/Redux/counter/counter.reducer.jsx
+++ redux-1/src/Redux/counter/counter.reducer.jsx
@@ -9,9 +9,9 @@ let initialState = {
 let counterReducer = ((state=initialState, action)=>{                  // reducer expecting two parameters i.e, state & action
     switch(action.type){
         case DECR :
-            return {qty : state.qty - 1}
+            return {...state, qty:state.qty - 1}
         case INCR : 
-        return {qty : state. qty + 1}    
+        return {...state, qty:state. qty + 1}    
         default : 
         return state
     }
