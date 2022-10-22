bfc3c3ebc71c30ea6b0f6285975abb2cb9e6a9e9
pavanprostack <pavanprostack@gmail.com>
2022-10-22 19:50:00 +0530

    ok

--- React-All/navigationapp/src/Product/ProductList.jsx
+++ React-All/navigationapp/src/Product/ProductList.jsx
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import Axios from 'axios'
 
@@ -9,6 +10,7 @@ const ProductList = () => {
     useEffect(()=>{
         let url='http://127.0.0.1:5000/api/products/'
         Axios.get(url).then((response)=>{
+            // console.log(response)
             setProducts(response.data);
         }).catch(()=>{})
     }, []);
@@ -16,19 +18,22 @@ const ProductList = () => {
 
   return <>
   <div className="container">
+    <pre>{JSON.stringify(products)}</pre>
     <div className="row">
         {
              products.length > 0 ? 
              <>
              {
                  products.map((product)=>{
-                     return   <div className="col-8">
+                     return   <div className="col-3">
                      <div className="card-header">
                          <img src={product.image} alt="img" />
                      </div>
                      <div className="card-body">
                         <ul className='list-group'>
-                            <li className='list-group-item'>{product.productName}</li>
+                            <li className='list-group-item'>{product.name}</li>
+                            <li className='list-group-item'>{product.price}</li>
+                            <li className='list-group-item'>{product.qty}</li>
                         </ul>
                      </div>
                  </div>
